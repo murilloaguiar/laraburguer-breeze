3d1582e215845d7c4a784c9961f2ea9922512f98
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-07-12 12:50:17 -0300

    feat: Adicionando layout

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,19 @@
+<template> 
+
+   <HomeLayoutVue>
+      <div style="height: 200px;"></div>
+   </HomeLayoutVue>
+
+
+</template>
+
+<script>
+   import HomeLayoutVue from "@/Layouts/HomeLayout.vue";
+
+   export default{
+      components:{
+         HomeLayoutVue
+      },
+
+   }
+</script>
