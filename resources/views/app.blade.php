c4d37f056af34721c33e2f5ef7fac948cc02d956
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-09-22 09:22:58 -0300

    Adicionar logo e favicon.
    Separar a navbar e rodapé em componentes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Laraburguer</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/x-icon">
 
         <!-- Scripts -->
         @routes
