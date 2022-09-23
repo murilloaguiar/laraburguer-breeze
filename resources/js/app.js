fcbda50413080fb93b5267f934f6a26d25b0f1ba
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-09-23 12:42:52 -0300

    Adicionar Link globalmente

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { createInertiaApp, Link } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
@@ -18,6 +18,7 @@ createInertiaApp({
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .component('HomeLayout', HomeLayout)
+            .component('Link', Link)
             .mount(el);
     },
 });
