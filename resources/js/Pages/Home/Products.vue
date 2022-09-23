8463b69c1749da430a678383f60e51cd9e19af6c
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-09-23 12:44:01 -0300

    Melhorar listagem de produtos
    Mostar produtos em cards, dividos por colunas. Adicionar um componente para paginação do resultados

--- resources/js/Pages/Home/Products.vue
+++ resources/js/Pages/Home/Products.vue
@@ -1,48 +1,123 @@
 <template>
     <div class="container mx-auto">
         <h1>Catalogo</h1>
-        <table class="w-full whitespace-nowrap">
-            <tr class="text-left font-bold">
-                <th class="pb-4 pt-6 px-6">Produto</th>
-                <th class="pb-4 pt-6 px-6">Categoria</th>
-                <th class="pb-4 pt-6 px-6" colspan="2">Preço</th>
-            </tr>
-            <tr
-                v-for="product in products.data"
-                :key="product.id"
-                class="hover:bg-gray-100 focus-within:bg-gray-100"
-            >
-                <td class="border-t">
 
-                    {{ product.name }}
-                    
-                </td>
-                <td class="border-t">
-                    
-                    {{ product.category_id }}
-                    
-                </td>
-                <td class="w-px border-t">
-                    
-                    {{ product.price }}
-                    
-                </td>
-            </tr>
-            <tr v-if="products.length === 0">
-                <td class="px-6 py-4 border-t" colspan="4">No products found.</td>
-            </tr>
-        </table>
+        <div class="grid grid-cols-4 gap-4">
+            <div
+                v-for="product in products.data" :key="product.id"
+                class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+            >
+                <a href="#">
+                    <img
+                        class="p-8 rounded-t-lg"
+                        src="assets/images/lanche-capa.jpg"
+                        alt="product image"
+                    />
+                </a>
+                <div class="px-5 pb-5">
+                    <a href="#">
+                        <h5
+                            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+                        >
+                            {{product.name}}
+                        </h5>
+                    </a>
+                    <div class="flex items-center mt-2.5 mb-5">
+                        <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title>First star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                            ></path>
+                        </svg>
+                        <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title>Second star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                            ></path>
+                        </svg>
+                        <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title>Third star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                            ></path>
+                        </svg>
+                        <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title>Fourth star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                            ></path>
+                        </svg>
+                        <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title>Fifth star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                            ></path>
+                        </svg>
+                        <span
+                            class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3"
+                            >5.0</span
+                        >
+                    </div>
+                    <div class="flex justify-between items-center">
+                        <span
+                            class="text-3xl font-bold text-gray-900 dark:text-white"
+                            >${{product.price}}</span
+                        >
+                        <a
+                            href="#"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >Add to cart</a
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <Pagination :links="products.links" class="mt-6" />
     </div>
 </template>
 
 <script>
-import HomeLayout from '@/Layouts/HomeLayout.vue';
+import HomeLayout from "@/Layouts/HomeLayout.vue";
+import Pagination from "../../Shared/Pagination.vue"
 
 export default {
-    props:{
-        products: Object
+    components:{
+        Pagination
     },
-    layout: HomeLayout
-    
-}
+    props: {
+        products: Object,
+    },
+    layout: HomeLayout,
+};
 </script>
---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- <div>
+        <Component
+            v-for="link in links"
+            :is="link.url ? 'Link' : 'span'"
+            :href="link.url"
+            v-html="link.label"
+            class="px-1"
+            :class="{ 'text-gray-500': !link.url, 'font-bold': link.active }"
+        />
+    </div> -->
+
+    <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        <div>
+            <nav class="isolate inline-flex -space-x-px rounded-lg shadow-sm" aria-label="Pagination">
+            
+                <Link
+                    v-for="(link, index) in links" :key="index"
+                    :href="link.url"
+                    class="relative inline-flex items-center px-4 py-2 text-sm font-medium hover:bg-blue-100"
+                    :class="{'rounded-l-md': index == 0, 'rounded-r-md': index == numberOfLinks-1, 'bg-blue-200 border-blue-300': link.active}"
+                    v-html="link.label"
+                ></Link>
+
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        links: Array,
+    },
+    computed:{
+        numberOfLinks(){
+            return this.links.length
+        },
+
+    }
+};
+</script>
